40be0bce9fbc6c8d9989b60fd6fc58069df97274
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-29 20:11:19 +0530

    Added a dataService

--- src/main/java/io/yadnyesh/springmockito/business/SomeBusinessImpl.java
+++ src/main/java/io/yadnyesh/springmockito/business/SomeBusinessImpl.java
@@ -1,11 +1,19 @@
 package io.yadnyesh.springmockito.business;
 
+import io.yadnyesh.springmockito.data.SomeDataService;
+
 import java.util.Arrays;
 
 public class SomeBusinessImpl {
 	
 	
+	SomeDataService someDataService;
+	
 	public int calculateSum(int[] data) {
 		return Arrays.stream(data).sum();
+	}
+	
+	public int calculateSumUsingDataService() {
+		return Arrays.stream(someDataService.retrieveAllData()).sum();
 	}
 }
---
+++ src/main/java/io/yadnyesh/springmockito/data/SomeDataService.java
@@ -0,0 +1,8 @@
+package io.yadnyesh.springmockito.data;
+
+public class SomeDataService {
+	
+	public int[] retrieveAllData() {
+		return new int[] {1,2,3,4};
+	}
+}
