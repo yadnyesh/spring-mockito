1ff06524497fe85b55beb869be8e4dbe82061110
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-29 19:46:53 +0530

    First 2 tests

--- src/test/java/io/yadnyesh/springmockito/business/SomeBusinessTest.java
+++ src/test/java/io/yadnyesh/springmockito/business/SomeBusinessTest.java
@@ -1,5 +1,25 @@
 package io.yadnyesh.springmockito.business;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class SomeBusinessTest {
+	
+	@Test
+	public void calculateSum_basic() {
+		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
+		int actualResult = someBusiness.calculateSum(new int[] {1,2,3,4,5});
+		int expectedResult = 15;
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void calculateSum_emptyArray() {
+		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
+		int actualResult = someBusiness.calculateSum(new int[] {});
+		assertEquals(0, actualResult);
+	}
+	
+	
 
 }
