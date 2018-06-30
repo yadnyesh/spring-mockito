15acdcea69891abc2dec6fc2e3022538bed4fdf1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-30 10:12:50 +0530

    Start using Mockito

---
+++ src/test/java/io/yadnyesh/springmockito/business/SomeBusinessMockTest.java
@@ -0,0 +1,37 @@
+package io.yadnyesh.springmockito.business;
+
+import io.yadnyesh.springmockito.data.SomeDataService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SomeBusinessMockTest {
+	
+	@Test
+	public void calculateSumUsingDataService_basic() {
+		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
+		someBusiness.setSomeDataService(new SomeDataServiceStub());
+		int actualResult = someBusiness.calculateSumUsingDataService();
+		int expectedResult = 6;
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void calculateSumUsingDataService_emptyArray() {
+		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
+		someBusiness.setSomeDataService(new SomeDataServiceEmptyStub());
+		int actualResult = someBusiness.calculateSumUsingDataService();
+		assertEquals(0, actualResult);
+	}
+	
+	@Test
+	public void calculateSumUsingDataService_oneValue() {
+		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
+		someBusiness.setSomeDataService(new SomeDataServiceOneElementStub());
+		int actualResult = someBusiness.calculateSumUsingDataService();
+		assertEquals(5, actualResult);
+	}
+	
+	
+
+}
