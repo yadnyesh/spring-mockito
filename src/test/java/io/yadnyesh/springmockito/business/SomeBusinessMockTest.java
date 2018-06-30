6331d732ecccb9e969072cf6638152424ee07da6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-30 10:34:49 +0530

    All tests with Mockito

--- src/test/java/io/yadnyesh/springmockito/business/SomeBusinessMockTest.java
+++ src/test/java/io/yadnyesh/springmockito/business/SomeBusinessMockTest.java
@@ -2,15 +2,19 @@ package io.yadnyesh.springmockito.business;
 
 import io.yadnyesh.springmockito.data.SomeDataService;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class SomeBusinessMockTest {
 	
 	@Test
 	public void calculateSumUsingDataService_basic() {
 		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
-		someBusiness.setSomeDataService(new SomeDataServiceStub());
+		SomeDataService someDataServiceMock = mock(SomeDataService.class);
+		when(someDataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+		someBusiness.setSomeDataService(someDataServiceMock);
 		int actualResult = someBusiness.calculateSumUsingDataService();
 		int expectedResult = 6;
 		assertEquals(expectedResult, actualResult);
@@ -19,7 +23,9 @@ public class SomeBusinessMockTest {
 	@Test
 	public void calculateSumUsingDataService_emptyArray() {
 		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
-		someBusiness.setSomeDataService(new SomeDataServiceEmptyStub());
+		SomeDataService someDataServiceMock = mock(SomeDataService.class);
+		when(someDataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+		someBusiness.setSomeDataService(someDataServiceMock);
 		int actualResult = someBusiness.calculateSumUsingDataService();
 		assertEquals(0, actualResult);
 	}
@@ -27,7 +33,9 @@ public class SomeBusinessMockTest {
 	@Test
 	public void calculateSumUsingDataService_oneValue() {
 		SomeBusinessImpl someBusiness = new SomeBusinessImpl();
-		someBusiness.setSomeDataService(new SomeDataServiceOneElementStub());
+		SomeDataService someDataServiceMock = mock(SomeDataService.class);
+		when(someDataServiceMock.retrieveAllData()).thenReturn(new int[] {5});
+		someBusiness.setSomeDataService(someDataServiceMock);
 		int actualResult = someBusiness.calculateSumUsingDataService();
 		assertEquals(5, actualResult);
 	}
