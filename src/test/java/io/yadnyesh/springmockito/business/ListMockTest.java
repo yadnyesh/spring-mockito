e9daabadf38c56b5a664215977b9d44a14c80c26
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-30 19:36:30 +0530

    Few more mock tests

--- src/test/java/io/yadnyesh/springmockito/business/ListMockTest.java
+++ src/test/java/io/yadnyesh/springmockito/business/ListMockTest.java
@@ -10,10 +10,24 @@ import static org.mockito.Mockito.when;
 
 public class ListMockTest {
 	
+	List mock = mock(List.class);
+	
 	@Test
-	public void Test() {
-		List mock = mock(List.class);
+	public void basicWithSize() {
 		when(mock.size()).thenReturn(5);
 		assertEquals(5, mock.size());
+	}
+	
+	@Test
+	public void returnDifferentValues() {
+		when(mock.size()).thenReturn(5).thenReturn(10);
+		assertEquals(5, mock.size());
+		assertEquals(10,mock.size());
+	}
+	
+	@Test
+	public void returnWithParameters() {
+		when(mock.get(0)).thenReturn("yadnyesh");
+		assertEquals("yadnyesh", mock.get(0));
 	}
 }
